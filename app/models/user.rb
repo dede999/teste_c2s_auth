198090f275af3f2d57bd3a0cd23089452cb891fd
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-08-13 17:07:54 -0300

    Create model methods
    I'm  doing that, so I can encapsulate user management logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   validates :password, presence: true
 
   has_encrypted :password
+
+  def create_token
+    JWT.encode({
+      user_id: id, expiration_date: Time.now + 2.hours
+    }, ENV['JWT_SECRET'], 'HS256')
+  end
+
+  def valid_password?(password)
+    User.decrypt_password_ciphertext(password_ciphertext) == password
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,41 @@ RSpec.describe User, type: :model do
       expect(valid_user).to be_valid
     end
   end
+
+  describe 'create_token' do
+    result = nil
+    let(:token) { 'user_token' }
+
+    before do
+      allow(JWT).to receive(:encode).and_return(token)
+      result = user.create_token
+    end
+
+    it 'returns a token' do
+      expect(result).to eq(token)
+    end
+
+    it 'is expected to call JWT.encode' do
+      expect(JWT).to have_received(:encode)
+    end
+  end
+
+  describe 'valid_password?' do
+    before do
+      allow(User).to receive(:decrypt_password_ciphertext).and_return('right')
+    end
+
+    %w[wrong right].each do |password|
+      context "when password is #{password}" do
+        it "is expected to have returned #{password == 'right'}" do
+          expect(user.valid_password?(password)).to eq(password == 'right')
+        end
+
+        it 'is expected to have decrypted password' do
+          user.valid_password?(password)
+          expect(User).to have_received(:decrypt_password_ciphertext).with(user.password_ciphertext)
+        end
+      end
+    end
+  end
 end
